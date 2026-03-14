885c27bdcc4b8da185a9be58270cfd40571bca1f
Akhil-Lanka09 <akhillanka00@gmail.com>
2026-03-14 23:13:57 +0530

    Delete RaHa-fixed directory

--- RaHa-fixed/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Allow images from any domain for product images
-  images: {
-    unoptimized: true,
-  },
-};
-
-module.exports = nextConfig;
